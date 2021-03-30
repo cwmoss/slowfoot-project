5000423548a0812fe6db42bba2f90c511582f11d
Robert Wagner <rw@20sec.net>
2021-03-30 21:10:16 +0200

    index page inline js removed

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,12 @@ $('document').ready(function() {
 	console.log("init")
 // 250 × 32 is "couldn't get image"
 	$('img.maybenot').on('load', function(){
-		console.log('loaded', this)
+		// console.log('loaded', this)
 		if(this.width!=250 && this.height != 32){
 			$(this).css("display", "block")
 		}
 	}).each(function(){
-		console.log("img", this)
+		// console.log("img", this)
 		$(this).prop("src", $(this).data("src"))
 	})
 
