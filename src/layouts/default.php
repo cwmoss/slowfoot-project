83305dda24220eafb6ba81446ff5bfec457b1d66
Robert Wagner <rw@20sec.net>
2021-03-31 15:25:15 +0200

    apache dev server, paths, cachebuster

--- src/layouts/default.php
+++ src/layouts/default.php
@@ -4,9 +4,9 @@
     <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
     <link rel="SHORTCUT ICON" href="/favicon.ico">
-    <link rel="stylesheet" href="<?=PATH_PREFIX?>/css/app.css" type="text/css">
-    <script src="<?=PATH_PREFIX?>/js/jquery-3.6.0.min.js"></script>
-    <script src="<?=PATH_PREFIX?>/js/app.js"></script>
+    <link rel="stylesheet" href="<?=path_asset('/css/app.css', true)?>" type="text/css">
+    <script src="<?=path_asset('/js/jquery-3.6.0.min.js')?>"></script>
+    <script src="<?=path_asset('/js/app.js')?>"></script>
     <title><?=$title?></title>
 
 </head>
@@ -14,7 +14,7 @@
     <header>
 <nav>
     <span>MUMOK</span>
-    <ul><li><a href="/">Artists</a></li>
+    <ul><li><a href="<?=path_page('/')?>">Artists</a></li>
     </ul>
 </nav>
 </header>
@@ -32,7 +32,5 @@
 </footer>
 
 </body>
-<?php
-//new HotReloader\HotReloader('//localhost:1199/phrwatcher.php');
-?>
+
 </html>
--- src/templates/work.php
+++ src/templates/work.php
@@ -1,22 +1,23 @@
 <?php
 layout('default');
 
-#$links = query('*[_id=="$id"]{articles[]->, pix[]->}[0]', ['id' =>$_id]);
-#var_dump($links);
+//$links = query('*[_id=="$id"]{articles[]->, pix[]->}[0]', ['id' =>$_id]);
+//var_dump($links);
 $links = [];
-$title = $page['originaltitle'] ?? $page['title_de'] ?? $page['title_en'] ?? 'k.a.'; 
+$title = $page['originaltitle'] ?? $page['title_de'] ?? $page['title_en'] ?? 'k.a.';
 ?>
 <article>
 <h1><?=$title?></h1>
 
-<?if($page['default_image']){?>
+<?if ($page['default_image']) {?>
 	<img class="maybenot" data-src="<?=$page['default_image']?>" style="display:none;">
 <?}?>
 
 <div class="a-content">
 
 
-<p><strong><?=$page['title_de']?:$page['originaltitle']?></strong><br><br><?=$page['description_de']?></p>
+
+<p><strong><?=$page['title_de'] ?: $page['originaltitle']?></strong><br><br><?=$page['description_de']?></p>
 
 <div class="material">
 	<?=$page['material_de']?> <?=$page['measurement_width']?> x <?=$page['measurement_height']?> x <?=$page['measurement_depth']?> <?=$page['measurement_unit']?>
@@ -36,23 +37,26 @@ $title = $page['originaltitle'] ?? $page['title_de'] ?? $page['title_en'] ?? 'k.
 </div>
 
 <div class="artists">
-	<?if($page['artists'])foreach($page['artists'] as $artref){
-		$art = $ref($artref);
-		
-		?>
+	<?if ($page['artists']) {
+    foreach ($page['artists'] as $artref) {
+        $art = $ref($artref); ?>
 		<section><a href="<?=$path($art)?>"> <?=$art['firstname']?> <?=$art['familyname']?></a></section>
-	<?}?>
+	<?
+    }
+}?>
 	</div>
 
 <div class="tags">
 	<ul class="tags">
-		<?#var_dump($page['tags']);?>
-		<?if($page['tags']) foreach($page['tags'] as $t){
-			$tag = $ref($t);
-			?>
+		<?//var_dump($page['tags']);?>
+		<?if ($page['tags']) {
+    foreach ($page['tags'] as $t) {
+        $tag = $ref($t); ?>
 			<li><a href="<?=$path($tag)?>"><?=$tag['title']?></a></li>
 			
-		<?}?>
+		<?
+    }
+}?>
 	</ul>
 	
 </div>
@@ -64,8 +68,8 @@ $title = $page['originaltitle'] ?? $page['title_de'] ?? $page['title_en'] ?? 'k.
 <?php
 /*
   "measurement_height":80.5,"inventorynr_sorted":"P 00001\/000","creditline":"erworben\/acquired in","title_en":"Construction Design for an Airport",
- 
-	   "in_exhibition":"yes","exploitation_rights":"Bildrecht, Wien","material_de":"Bronze, Glas","measurement_depth":85,
-	   "originaltitle":"Konstruktion f\u00fcr einen Flughafen","default_image":"https:\/\/www.mumok.at\/imageobject.php?objid=1","inventorynr":"P 1\/0","measurement_unit":"cm"}
+
+       "in_exhibition":"yes","exploitation_rights":"Bildrecht, Wien","material_de":"Bronze, Glas","measurement_depth":85,
+       "originaltitle":"Konstruktion f\u00fcr einen Flughafen","default_image":"https:\/\/www.mumok.at\/imageobject.php?objid=1","inventorynr":"P 1\/0","measurement_unit":"cm"}
 */
 ?>
