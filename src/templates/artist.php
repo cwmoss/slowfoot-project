6528588df930b2eea295c3fcaadd5e632b88dedc
Robert Wagner <rw@20sec.net>
2024-08-19 17:49:06 +0200

    move assets

--- src/templates/artist.php
+++ src/templates/artist.php
@@ -7,32 +7,31 @@ $subtitle = 'untertitel';
 // $works = $query(['_type' => 'work', 'artists._ref' => $_id]);
 //var_dump($works);
 
-if ($page['works']) {
+if ($page['works'] ?? null) {
     $works = array_map(function ($w) use ($ref) {
         return $ref($w);
     }, $page['works']);
 }
 ?>
 <article>
-<h1><?=$page['firstname']?> <?=$page['familyname']?>  </h1>
+    <h1><?= $page['firstname'] ?> <?= $page['familyname'] ?> </h1>
 
-<div class="a-content">
-<p>geb. <?=$page['birthyear']?> <?=$page['cityofbirth_de']?>  
-    <?if ($page['deathyear']) {?><br>gest. <?=$page['deathyear']?>  <?=$page['cityofdeath_en']?><?}?>
-</p>
+    <div class="a-content">
+        <p>geb. <?= $page['birthyear'] ?> <?= $page['cityofbirth_de'] ?>
+            <? if ($page['deathyear']) { ?><br>gest. <?= $page['deathyear'] ?> <?= $page['cityofdeath_en'] ?><? } ?>
+        </p>
 
-<p><?=markdown($text)?></p>
 
-<ul>
-	<?if ($works) {
-    foreach ($works as $work) {
-        $title = $work['originaltitle'] ?? $work['title_de'] ?? $work['title_en'] ?? 'k.a.'; ?>
-		<li><a href="<?=$path($work)?>"><?=$title?></a></li>
-	<?php
-    }
-}?>
-</ul>
-</div>
+        <ul>
+            <? if ($works) {
+                foreach ($works as $work) {
+                    $title = $work['originaltitle'] ?? $work['title_de'] ?? $work['title_en'] ?? 'k.a.'; ?>
+                    <li><a href="<?= $path($work) ?>"><?= $title ?></a></li>
+            <?php
+                }
+            } ?>
+        </ul>
+    </div>
 
 
 </article>
